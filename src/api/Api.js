3e454c7f22c18c7fd099e1b045b23de93d8f96dd
alejandroodom <113924039+alejandroodom@users.noreply.github.com>
2025-02-17 12:00:43 +0100

    Primer commit del front.

---
+++ src/api/Api.js
@@ -0,0 +1,17 @@
+
+const API_URL = import.meta.env.VITE_API_URL; // Backend en Render
+
+fetch(`${API_URL}/auth/register`, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        username: 'ejemplo',
+        email: 'ejemplo@mail.com',
+        password: '123456'
+    })
+})
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
